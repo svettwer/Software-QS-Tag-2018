49ec7ad8b2e3b8411555345328acbe35690fa84a
Sven Hettwer <sven.hettwer@consol.de>
2018-09-01 11:41:12 +0200

    Added config for offline spring s2i image

--- paas/bootstrap.sh
+++ paas/bootstrap.sh
@@ -12,3 +12,6 @@ oc login -u system:admin
 
 oc new-project gogs
 oc new-app -f http://bit.ly/openshift-gogs-template --param=HOSTNAME=gogs.$(minishift ip).nip.io
+
+oc project openshift
+oc process -f $(git rev-parse --show-toplevel)/paas/yml/s2i_spring_offline_build_template.yml | oc apply -f -
