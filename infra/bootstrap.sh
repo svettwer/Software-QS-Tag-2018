010de6b61c2dd2ea47038ca0b078c25a078d8a03
Sven Hettwer <sven.hettwer@consol.de>
2018-09-03 15:48:50 +0200

    Added Jenkins

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -5,3 +5,6 @@ oc login -u developer -p whateverYourPasswordWouldBe
 oc new-project todo-app-dev
 oc new-project todo-app-int
 oc new-project todo-app-prod
+
+oc project todo-app-dev
+oc process -f infra/yml/jenkins_template.yml | oc apply -f -
