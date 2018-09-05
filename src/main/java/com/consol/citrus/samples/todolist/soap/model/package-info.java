67826f6e62f525dc02c25b06f6c015fda784a2e3
Sven Hettwer <sven.hettwer@consol.de>
2018-09-05 12:57:38 +0200

    Added sample app code to the project

---
+++ src/main/java/com/consol/citrus/samples/todolist/soap/model/package-info.java
@@ -0,0 +1,9 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2018.06.14 um 04:12:20 PM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://citrusframework.org/samples/todolist", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.consol.citrus.samples.todolist.soap.model;
