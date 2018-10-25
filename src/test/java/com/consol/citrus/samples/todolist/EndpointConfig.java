b244f8d42abe34462ceffa5b89db23e82ecd6e34
Sven Hettwer <sven.hettwer@consol.de>
2018-10-25 16:24:48 +0200

    Changed app url for citrus tests

--- src/test/java/com/consol/citrus/samples/todolist/EndpointConfig.java
+++ src/test/java/com/consol/citrus/samples/todolist/EndpointConfig.java
@@ -31,7 +31,7 @@ public class EndpointConfig {
     public HttpClient todoClient() {
         return CitrusEndpoints.http()
                             .client()
-                            .requestUrl("http://todo-app-int.192.168.99.101.nip.io")
+                            .requestUrl("http://todo-app-int.us-east-1.elasticbeanstalk.com")
                             .build();
     }
 }
