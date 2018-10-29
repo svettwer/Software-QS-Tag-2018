2c90e6f26bf450313639b4d1835c3717ef1e4e34
Sven Hettwer <sven.hettwer@consol.de>
2018-10-29 15:29:39 +0100

    Removed OpenShift specific parts and End-2-End Tests

--- infra/snippets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oc run sakuli-s2i-testsuite \
-    --image=172.30.1.1:5000/todo-app-int/sakuli-s2i-testsuite:latest \
-    --attach=true \
-    --rm \
-    --restart=Never \
-    -l application=sakuli-s2i-testsuite \
-    -n todo-app-int
